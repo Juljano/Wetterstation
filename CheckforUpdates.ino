1df0067ac940adba0a0accbcedc0f53060fb0bde
Mr1000 <73438118+Juljano@users.noreply.github.com>
2023-07-09 12:20:10 +0200

    Version 3.5 - final Version

--- CheckforUpdates.ino
+++ CheckforUpdates.ino
@@ -1,1 +1,60 @@
+void update_started() {
+  Serial.println("CALLBACK: HTTP update process started");
+}
 
+void update_finished() {
+  Firebase.pushString(fb, "/executed Updates/", "Version " + String(currentVersion) + " ist installiert!");
+}
+
+void update_error(int err) {
+  //Firebase.pushString(fb, "/Updates failed/", String(err));
+}
+
+void getUpdateFile() {
+  client.setInsecure();
+  ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW);
+  ESPhttpUpdate.onStart(update_started);
+  ESPhttpUpdate.onEnd(update_finished);
+  ESPhttpUpdate.onError(update_error);
+  t_httpUpdate_return ret = ESPhttpUpdate.update(client, "https://www.schrotthandel-moeller.de/wetter/Main.ino.nodemcu.bin");
+  Serial.println(ret == HTTP_UPDATE_OK ? "HTTP_UPDATE_OK" : "HTTPS_UPDATE_FAILD Error: " + ESPhttpUpdate.getLastErrorString());
+}
+
+void checkForUpdate() {
+  Serial.println("Current Version: " + String(currentVersion));
+  client.setInsecure();
+
+  if (client.connect("www.schrotthandel-moeller.de", 443)) {
+    Serial.println("Connected to server");
+    client.print(String("GET ") + "/wetter/Version.txt" + " HTTP/1.1\r\n" +
+                 "Host: " + "www.schrotthandel-moeller.de" + "\r\n" +
+                 "Connection: close\r\n\r\n");
+
+    while (client.connected()) {
+      String line = client.readStringUntil('\n');
+      line.trim();
+
+      if (line == "") {
+        break;
+      }
+    }
+
+    String versionString = client.readStringUntil('\n');
+    versionString.trim();
+    latestVersion = versionString.toFloat();
+
+    client.stop();
+  }
+
+  if (latestVersion > currentVersion) {
+    Serial.println("New version available. Starting update...");
+    getUpdateFile();
+    
+  }else if(latestVersion == currentVersion){
+    Serial.println("You are already using the latest version.");
+    
+    }else {
+  
+    Serial.println("No update available.");
+  }
+}
--- Main.ino
+++ Main.ino
@@ -3,24 +3,31 @@
 #include <Adafruit_MLX90614.h>
 #include  <Adafruit_BMP280.h>
 #include "DHT.h" 
+#include <ESP8266httpUpdate.h>
 #include <ESP8266HTTPClient.h>
 #define DHT_TYPE DHT22 
-
+#include <OneWire.h>
+#include <DallasTemperature.h>
 #define FIREBASE_HOST "https://wetterstation-9a964-default-rtdb.europe-west1.firebasedatabase.app/" 
 #define FIREBASE_AUTH "AIzaSyCV2oxI6kTA2i34GOublb2xbL1syrrppx0"
 #define WIFI_SSID "Fritz Routi" 
 #define WIFI_PASSWORD "Elias01.12.2020" 
 const char* host = "www.schrotthandel-moeller.de";
 const int httpsPort = 443;
-int batteryinPercent;
+float currentVersion = 3.5;
+float latestVersion;
 double dewPoint;
-
 FirebaseData fb;
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 
 Adafruit_BMP280 bmp; 
-
+WiFiClientSecure client;
 const int DHT_PIN = 14; 
+#define ONE_WIRE_BUS D7
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
 DHT dht(DHT_PIN, DHT_TYPE);
 
 void setup() {
@@ -43,13 +50,14 @@ while (WiFi.status() != WL_CONNECTED)
   }
 }
 
-  Serial.println("Verbunden!");
+  Serial.println("Verbunden!");                                                
+  pinMode(LED_BUILTIN, OUTPUT);
 
-  
- Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 
   //Enable auto reconnect the WiFi when connection lost
    Firebase.reconnectWiFi(true);
+
 
 
   /* Default settings from datasheet.  */
@@ -59,41 +67,53 @@ while (WiFi.status() != WL_CONNECTED)
                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                   Adafruit_BMP280::STANDBY_MS_500);  /* Standby time. */
 
+  
 
+   sensorState();
 
-  dht.begin();
-  mlx.begin();
-  bmp.begin();
+  checkForUpdate();
 
 
 }
 
 void loop() {
 
+    sensors.requestTemperatures(); // Send the command to get temperatures
+
+
+   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);   
+
   //calculate the dewPoint
-  double gamma = log(dht.readHumidity() / 100) + ((17.62 * bmp.readTemperature()) / (243.5 + bmp.readTemperature()));
+  double gamma = log(dht.readHumidity() / 100) + ((17.62 *  sensors.getTempCByIndex(0)) / (243.5 +  sensors.getTempCByIndex(0)));
   dewPoint = 243.5 * gamma / (17.62 - gamma);
 
-  batteryinPercent = batteryCheck(A0); //get Percent from Battery back
-  callWebsite();
   updateFirebase();
-
-   
-   
+  startdeepSleep();
 }
 
 
 void updateFirebase() {
-  Firebase.pushFloat(fb, "/MLX/Wolkentemperatur",mlx.readObjectTempC());
+  
   Firebase.pushFloat(fb, "/DHT22/Temperature",dht.readTemperature());
   Firebase.pushFloat(fb, "/DHT22/Humidity", dht.readHumidity());
   Firebase.pushFloat(fb, "/BMP280/Pressure", bmp.readPressure()/100000);
-  Firebase.pushFloat(fb, "/BMP280/Temperature", bmp.readTemperature());
-  Firebase.pushFloat(fb, "/Altitude BMP", bmp.readAltitude(1013.25));
-  Firebase.pushFloat(fb, "Others/dew Point", dewPoint);
-  Firebase.pushFloat(fb, "Station/Battery" , batteryinPercent);
+  Firebase.pushFloat(fb, "/BMP280/Temperature", bmp.readTemperature()-1.0);
+  Firebase.pushFloat(fb, "/BMP280/Altitude", bmp.readAltitude(1021.04));
+  Firebase.pushFloat(fb, "/Others/dew Point", dewPoint);
+  Firebase.pushFloat(fb, "/DS18b20/Temperature", sensors.getTempCByIndex(0));
+  Firebase.pushFloat(fb, "/Others/Heat index", dht.computeHeatIndex(sensors.getTempCByIndex(0), dht.readHumidity(), false));
 
-  startdeepSleep();
+    Serial.println();
+
+  Serial.println(mlx.readObjectTempC());
+  Firebase.pushFloat(fb, "/MLX90614/Cloudtemperature",mlx.readObjectTempC());
+  Firebase.pushFloat(fb, "/MLX90614/Temperature in C",mlx.readObjectTempC());
+  Firebase.pushFloat(fb, "/MLX90614/Temperature in F",mlx.readObjectTempF());
+
+
 }
 
 void startdeepSleep(){
@@ -102,6 +122,39 @@ void startdeepSleep(){
    Serial.println("Tiefschlaf-Modus");
    ESP.deepSleep(1800*1e6); //Für eine halbe Stunde geht der ESP schlafen
   
+}
 
- 
+void sensorState() {
+  sensors.begin();
+  dht.begin();
+
+  float tempC = sensors.getTempCByIndex(0);
+  bool allSensorsWorking = true;
+
+  if (tempC == DEVICE_DISCONNECTED_C) {
+    Firebase.setString(fb, "/sensorStates/DS18b20", "Der DS18b20 funktioniert nicht!");
+    allSensorsWorking = false;
+  }
+
+  if (!mlx.begin()) {
+    Firebase.setString(fb, "/sensorStates/MLX90614", "Der MLX90614 funktioniert nicht!");
+    allSensorsWorking = false;
+  }
+
+  if (!bmp.begin()) {
+    Firebase.setString(fb, "/sensorStates/BMP", "Der BMP280 funktioniert nicht!");
+    allSensorsWorking = false;
+  }
+
+  if (isnan(dht.readTemperature()) || isnan(dht.readHumidity())) {
+    Firebase.setString(fb, "/sensorStates/DHT22", "Der DHT22 funktioniert nicht!");
+    allSensorsWorking = false;
+  }
+
+  if (allSensorsWorking) {
+    Firebase.setString(fb, "/sensorStates/DS18b20", "OK");
+    Firebase.setString(fb, "/sensorStates/MLX90614", "OK");
+    Firebase.setString(fb, "/sensorStates/BMP", "OK");
+    Firebase.setString(fb, "/sensorStates/DHT22", "OK");
+  }
 }
